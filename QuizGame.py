96fd17ccf62488b5efd0a46abb9c651190f61b88
mika tiihonen <koneautomaatio@hotmail.com>
2023-09-25 16:34:01 +0300

    Dictionary and list of lists

--- QuizGame.py
+++ QuizGame.py
@@ -11,3 +11,15 @@ def display_score():
 def play_again():
     pass
 #-------------------------------
+
+questions = {
+  "Who created Python?: ": "A",
+  "What year was Python created?: ": "B",
+  "Python is tributed to which comedy group?: ": "C",
+  "Is the Earth round?: ": "A"
+  }
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerberg"],
+           ["A. 1989", "B. 1991", "C. 2000", "D. 2018"],
+           ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+           ["A. True", "B. False", "C. sometimes", "D. Whats's Earth?"]]
