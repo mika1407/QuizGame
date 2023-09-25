189de52aa8573adba7ac8a111b665aab5d3b6d5b
mika tiihonen <koneautomaatio@hotmail.com>
2023-09-25 20:54:48 +0300

    display score

--- QuizGame.py
+++ QuizGame.py
@@ -1,12 +1,47 @@
 #-------------------------------
 def new_game():
-    pass
+    
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+
+    for key in questions:
+        print("----------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter (A, B, C, or D): ")
+        guess = guess.upper()
+        guesses.append(guess)
+
+        correct_guesses += check_answer(questions.get(key),guess)
+        question_num += 1
+
+    display_score(correct_guesses, guesses)
 #-------------------------------
-def check_answer():
-    pass
+def check_answer(answer, guess):
+    
+    if answer == guess:
+        print("CORRECT!")
+        return 1
+    else:
+        print("WRONG!")
+        return 0
 #-------------------------------
-def display_score():
-    pass
+def display_score(correct_guesses, guesses):
+    print("----------------------")
+    print("RESULTS")
+    print("----------------------")
+
+    print("Answers: ", end="")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+
+    print("Guesses: ", end="")
+    for i in guesses:
+        print(i, end=" ")
+    print()
 #-------------------------------
 def play_again():
     pass
